f486b0cdb7fe97f669024d7ca6289e922966c75d
ajlopez <webmaster@ajlopez.com>
2016-02-04 09:01:03 -0300

    using otherwise

--- test/reducers.js
+++ test/reducers.js
@@ -38,3 +38,18 @@ exports['add two reducer using when'] = function (test) {
     test.equal(r(9, { type: DECREMENT }), 8);
     test.equal(r(42, 'foo'), 42);
 };
+
+exports['using otherwise'] = function (test) {
+    var r = reduman()
+        .when({ type: INCREMENT }, function (state, data) { return state + 1 })
+        .when({ type: DECREMENT }, function (state, data) { return state - 1 })
+        .otherwise(function (state, data) { return 0; });
+    
+    test.ok(r);
+    test.equal(typeof r, 'function');
+    
+    test.equal(r(0, { type: INCREMENT }), 1);
+    test.equal(r(9, { type: DECREMENT }), 8);
+    test.equal(r(42, 'foo'), 0);
+};
+
