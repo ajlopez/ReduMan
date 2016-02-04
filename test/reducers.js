5a80b4ef391fe66ec230a2a95a63fee71bc72f21
ajlopez <webmaster@ajlopez.com>
2016-02-04 08:57:06 -0300

    add reducer using when

--- test/reducers.js
+++ test/reducers.js
@@ -1,6 +1,9 @@
 
 var reduman = require('..');
 
+var INCREMENT = 1;
+var DECREMENT = -1;
+
 exports['create and invoke empty reducer'] = function (test) {
     var r = reduman();
     
@@ -13,5 +16,13 @@ exports['create and invoke empty reducer'] = function (test) {
     test.equal(result, 42);
 };
 
-
+exports['add reducer using when'] = function (test) {
+    var r = reduman().when({ type: INCREMENT }, function (state, data) { return state + 1 });
+    
+    test.ok(r);
+    test.equal(typeof r, 'function');
+    
+    test.equal(r(0, { type: INCREMENT }), 1);
+    test.equal(r(42, 'foo'), 42);
+};
 
